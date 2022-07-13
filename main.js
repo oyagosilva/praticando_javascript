5f2e10799d1462eac35cdd2c7ca5d9b95dcb560a
Yago Silva <100629090+oyagosilva@users.noreply.github.com>
2022-07-13 17:11:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML + "<b>Obrigado por clicar<b>"
+
+}
+
+
+/*function soma (n1, n2){
+    return n1 + n2;
+
+
+var validar = 0;
+
+function validaridade (idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }    
+    return validar;
+
+}
+
+
+//var idade = 18;
+//if (idade >= 18);
+    //alert("maior de idade");
+//else(
+    //alert("menor de idade");
+
+//);
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+//var lista = ["maça", "laranja", "limão"];
+//lista.push("laranja");
+//lista.pop();
+
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var idade = 20;
+//var nome = "Yago Silva";
+//var idade2 = 10;
+//var frase = "Liverpool é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase());
+*/
